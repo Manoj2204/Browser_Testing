6b0c3e16985c5c918854a11866c639ad8fc778c9
Manoj kumar <129170181+Manoj2204@users.noreply.github.com>
2023-11-29 16:00:48 +0530

    Update Launch_test.java

--- src/test/java/Demo/Launch_test.java
+++ src/test/java/Demo/Launch_test.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Launch_test 
+public class Launch_Test 
 {
 	@Test
 	public void test()
